10b06c1dcb1c958517217ced2f3682d62b0efaf9
Roman-Bobrovskiy <83470055+Roman-Bobrovskiy@users.noreply.github.com>
2021-06-28 14:03:38 +0300

    Add files via upload
    fix2

---
+++ gallery-items.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    description: "Rescue",
+    preview: "./img/Rescue.jpg",
+    original: "./img/Rescue.jpg",
+    style: "obj12",
+  },
+  {
+    description: "Scooter2",
+    preview: "./img/Scooter2_1.jpg",
+    original: "./img/Scooter2_1.jpg",
+    style: "obj11",
+  },
+  {
+    description: "Mystic4",
+    preview: "./img/Mystic4.jpg",
+    original: "./img/Mystic4.jpg",
+    style: "obj10",
+  },
+  {
+    description: "Scooter2",
+    preview: "./img/small/DSC03702_s.jpg",
+    original: "./img/DSC03702.jpg",
+    style: "obj9",
+  },
+  {
+    description: "Slalom",
+    preview: "./img/small/IMG_4416_s.jpg",
+    original: "./img/IMG_4416.jpg",
+    style: "obj8",
+  },
+  {
+    description: "Scout XXL",
+    preview: "./img/small/IMG_3837_s.jpg",
+    original: "./img/IMG_3837.jpg",
+    style: "obj7",
+  },
+  {
+    description: "Moscito4",
+    preview: "./img/small/DSC02896_s.jpg",
+    original: "./img/DSC02896.jpg",
+    style: "obj6",
+  },
+  {
+    description: "Escort3",
+    preview: "./img/G0035776.JPG",
+    original: "./img/G0035776.JPG",
+    style: "obj5",
+  },
+  {
+    description: "Moscito4",
+    preview: "./img/small/IMG_4426_s.JPG",
+    original: "./img/IMG_4426.JPG",
+    style: "obj4",
+  },
+  {
+    description: "Scotch",
+    preview: "./img/IMG_3175.jpg",
+    original: "./img/IMG_3175.jpg",
+    style: "obj3",
+  },
+  {
+    description: "Moscito4",
+    preview: "/img/Moscito4.jpg",
+    original: "/img/Moscito4.jpg",
+    style: "obj2",
+  },
+  {
+    description: "Me",
+    preview: "./img/IMG_4488.JPG",
+    original: "./img/IMG_4488.JPG",
+    style: "obj1",
+  },
+];
